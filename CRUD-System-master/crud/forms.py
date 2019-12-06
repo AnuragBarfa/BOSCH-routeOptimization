b89ab2ed7b649d6d2bacd513b954c884a1afc4a4
Shubham gupta Ggps <gptshubham595@gmail.com>
2019-12-06 17:57:20 +0530

    Book Done👍👍

--- CRUD-System-master/crud/forms.py
+++ CRUD-System-master/crud/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Book
 
 class BookForm(forms.ModelForm):
-	
+	publication_date = forms.DateTimeInput()
 	class Meta:
 		model = Book
 		fields = ('location','no_of_students', )
--- CRUD-System-master/crud/models.py
+++ CRUD-System-master/crud/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Book(models.Model):
 
     location = models.CharField(max_length=60)
-    no_of_students = models.IntegerField(blank=True, null=True)
+    no_of_students = models.IntegerField()
 
     def __str__(self):
         return self.location
