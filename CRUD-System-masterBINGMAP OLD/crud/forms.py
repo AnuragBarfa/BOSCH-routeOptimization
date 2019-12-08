967b20b6621203b8a0be6fb7a42800ae0c10a11d
Shubham gupta Ggps <gptshubham595@gmail.com>
2019-12-08 12:29:38 +0530

    Crud Check

--- CRUD-System-masterBINGMAP OLD/crud/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Book
-
-class BookForm(forms.ModelForm):
-	publication_date = forms.DateTimeInput()
-	class Meta:
-		model = Book
-		fields = ('location','no_of_students','timestamp', )
