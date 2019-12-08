9b437a43989d74aa15190d9b344d396a19bc2c6f
Shubham gupta Ggps <gptshubham595@gmail.com>
2019-12-08 14:57:24 +0530

    GMAP Final Form COMPLETE

--- CRUD-System-master/crud/forms.py
+++ CRUD-System-master/crud/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Book
 
 class BookForm(forms.ModelForm):
-	publication_date = forms.DateTimeInput()
 	class Meta:
 		model = Book
 		fields = ('location','no_of_students','timestamp', )
--- CRUD-System-master/crud/models.py
+++ CRUD-System-master/crud/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Book(models.Model):
-
     location = models.CharField(max_length=60)
     timestamp = models.CharField(max_length=60)
     no_of_students = models.IntegerField()
+
 
     def __str__(self):
         return self.location
