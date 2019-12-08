f41f06e67b0ccb7575fee9ac01dcc70d7f3a37d0
Shubham gupta Ggps <gptshubham595@gmail.com>
2019-12-08 12:22:28 +0530

    initial

--- BOSCH/home/models.py
+++ BOSCH/home/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 
-class Book(models.Model):
-
-    BOOK_TYPES = (
-        (u"Hardcover", u'Hardcover'),
-        (u"Paperback", u'Paperback'),
-        (u"E-book", u'E-book'),
-    )
-    title = models.CharField(max_length=50)
-    publication_date = models.DateField(null=True)
-    author = models.CharField(max_length=30, blank=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    pages = models.IntegerField(blank=True, null=True)
-    book_type = models.CharField(choices=BOOK_TYPES,max_length=120)
-
-    def __str__(self):
-        return self.title
+# Create your models here.
--- BOSCH/home/urls.py
+++ BOSCH/home/urls.py
@@ -6,9 +6,4 @@ urlpatterns=[
     url(r'^map/$',views.mapView,name='mapView'),
     url(r'^maptry/$',views.mapView,name='mapmark'),
     url(r'^graph/$',views.home,name='home'),
-    url(r'^books/$', views.book_list, name='book_list'),
-    url(r'^books/create$', views.book_create, name='book_create'),
-    url(r'^books/(?P<id>\d+)/update$', views.book_update, name='book_update'),
-    url(r'^books/(?P<id>\d+)/delete$', views.book_delete, name='book_delete'),
-
 ]
