13e4c2850088a1ce92d85c48a512567effacbd2f
Shubham gupta Ggps <gptshubham595@gmail.com>
2019-12-16 13:08:04 +0530

    Bosch name solved

--- How-To-Execute-CRUD-Using-Django-Ajax-and-JSONa/crud_ajax/views.py
+++ How-To-Execute-CRUD-Using-Django-Ajax-and-JSONa/crud_ajax/views.py
@@ -40,6 +40,8 @@ def create_distance_matrix(data):
     # Send q requests, returning max_rows rows per request.
     for i in range(q):
         origin_addresses = addresses[i * max_rows: (i + 1) * max_rows]
+        print("ORIGIN ADD===========================")
+        print(origin_addresses)
         response = send_request(origin_addresses, dest_addresses, API_key)
         distance_matrix += build_distance_matrix(response)
 
@@ -97,6 +99,7 @@ class RouteView(View):
         print(locations)
         print("BUS=====================")
         print(busdetails)
+
         passengerPerStop=[]
         busCapacity=[]
         dataForSolver={}
@@ -110,7 +113,7 @@ class RouteView(View):
             dataForDistanceMatrix['addresses'].append(x)
             passengerPerStop.append(int(locations[i]['count']))
 
-        dataForDistanceMatrix['addresses']=list(set(dataForDistanceMatrix['addresses']))    
+        dataForDistanceMatrix['addresses']=list(dataForDistanceMatrix['addresses'])
         
         print(dataForDistanceMatrix)
         
@@ -135,83 +138,6 @@ class RouteView(View):
         dataForSolver['ends'] = ends
         dataForSolver['max_allowed_time'] = 700
         dataForSolver['soft_time_windows'] = dataForSolver['time_windows']
-        # dataForSolver['distance_matrix']=distance_matrix
-    #     dataForSolver['distance_matrix'] = [
-    #     [
-    #         0, 548, 776, 696, 582, 274, 502, 194, 308, 194, 536, 502, 388, 354,
-    #         468, 776, 662
-    #     ],
-    #     [
-    #         548, 0, 684, 308, 194, 502, 730, 354, 696, 742, 1084, 594, 480, 674,
-    #         1016, 868, 1210
-    #     ],
-    #     [
-    #         776, 684, 0, 992, 878, 502, 274, 810, 468, 742, 400, 1278, 1164,
-    #         1130, 788, 1552, 754
-    #     ],
-    #     [
-    #         696, 308, 992, 0, 114, 650, 878, 502, 844, 890, 1232, 514, 628, 822,
-    #         1164, 560, 1358
-    #     ],
-    #     [
-    #         582, 194, 878, 114, 0, 536, 764, 388, 730, 776, 1118, 400, 514, 708,
-    #         1050, 674, 1244
-    #     ],
-    #     [
-    #         274, 502, 502, 650, 536, 0, 228, 308, 194, 240, 582, 776, 662, 628,
-    #         514, 1050, 708
-    #     ],
-    #     [
-    #         502, 730, 274, 878, 764, 228, 0, 536, 194, 468, 354, 1004, 890, 856,
-    #         514, 1278, 480
-    #     ],
-    #     [
-    #         194, 354, 810, 502, 388, 308, 536, 0, 342, 388, 730, 468, 354, 320,
-    #         662, 742, 856
-    #     ],
-    #     [
-    #         308, 696, 468, 844, 730, 194, 194, 342, 0, 274, 388, 810, 696, 662,
-    #         320, 1084, 514
-    #     ],
-    #     [
-    #         194, 742, 742, 890, 776, 240, 468, 388, 274, 0, 342, 536, 422, 388,
-    #         274, 810, 468
-    #     ],
-    #     [
-    #         536, 1084, 400, 1232, 1118, 582, 354, 730, 388, 342, 0, 878, 764,
-    #         730, 388, 1152, 354
-    #     ],
-    #     [
-    #         502, 594, 1278, 514, 400, 776, 1004, 468, 810, 536, 878, 0, 114,
-    #         308, 650, 274, 844
-    #     ],
-    #     [
-    #         388, 480, 1164, 628, 514, 662, 890, 354, 696, 422, 764, 114, 0, 194,
-    #         536, 388, 730
-    #     ],
-    #     [
-    #         354, 674, 1130, 822, 708, 628, 856, 320, 662, 388, 730, 308, 194, 0,
-    #         342, 422, 536
-    #     ],
-    #     [
-    #         468, 1016, 788, 1164, 1050, 514, 514, 662, 320, 274, 388, 650, 536,
-    #         342, 0, 764, 194
-    #     ],
-    #     [
-    #         776, 868, 1552, 560, 674, 1050, 1278, 742, 1084, 810, 1152, 274,
-    #         388, 422, 764, 0, 798
-    #     ],
-    #     [
-    #         662, 1210, 754, 1358, 1244, 708, 480, 856, 514, 468, 354, 844, 730,
-    #         536, 194, 798, 0
-    #     ],
-    # ]
-
-        # dataForSolver['passengerCount']= [0, 0, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8]
-        # dataForSolver['busCapacity']=[20, 5, 10, 35]
-        # dataForSolver['time_windows']=[(0,200)]*17
-    #     output=solver(dataForSolver)
-    #     results = output['routes']
         
     #     dataForSolver['distance_matrix']=distance_matrix
     #     dataForSolver['distance_matrix'] = [
